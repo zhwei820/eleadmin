841c1cd639a185aa62c95ae4fc11151dcc0e6a56
zhouwei <zhouwei@dianjoy.com>
2016-11-29 15:37:27 +0800

    add

---
+++ echo.php
@@ -0,0 +1,15 @@
+<?php
+$a = [];
+for ($i = 0; $i < 100; $i++) {
+    $tmp = [];
+    $tmp["date"] = '2016-05-03';
+
+    $tmp["name"] = '王小虎_' . $i;
+    $tmp["province"] = '上海';
+    $tmp["city"] = '普陀区';
+    $tmp["address"] = '上海市普陀区金沙江路 1518 弄';
+    $tmp["zip"] = 20033;
+
+    $a[] = $tmp;
+}
+echo json_encode($a, JSON_UNESCAPED_UNICODE);
