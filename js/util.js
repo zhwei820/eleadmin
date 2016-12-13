c1aa4abc71779d7321246b5e42c95dfd58a6edd0
zhouwei <zhouwei@dianjoy.com>
2016-12-13 16:36:37 +0800

    fix border

--- js/util.js
+++ js/util.js
@@ -50,7 +50,7 @@ function export_excel (rows, charset, type) {
     var tpl = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:{{type}}" xmlns="http://www.w3.org/TR/REC-html40">';
     tpl += '<head><meta charset="{{charset}}" /><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>';
     tpl += '表格1</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]-->';
-    tpl += '</head><body><table>{{table}}</table></body></html>';
+    tpl += '</head><body><table border="1 solid black">{{table}}</table></body></html>';
 
     var office = '';
     var maph = [
